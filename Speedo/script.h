99f32b3cc66cb1b62a5a6c7213e764be5499a46c
E66666666 <emily66666666ok@outlook.com>
2017-12-20 20:15:30 +0100

    Fix version number
    Relying on Manual Transmission and just needing to recompile other
    projects makes me keep forgetting to update the version numbers in the
    other mods, like this one >_<

--- Speedo/script.h
+++ Speedo/script.h
@@ -6,7 +6,7 @@ http://dev-c.com
 
 #pragma once
 
-#define DISPLAY_VERSION "v0.0.4"
+#define DISPLAY_VERSION "v0.0.5"
 
 #include "../../ScriptHookV_SDK/inc/natives.h"
 #include "../../ScriptHookV_SDK/inc/main.h"
